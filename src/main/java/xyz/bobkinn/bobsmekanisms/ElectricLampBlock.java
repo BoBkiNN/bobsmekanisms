b7c7da0920cc20b0cce56565c98fdaa0bad3716b
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-05-02 18:37:26 +0500

    fix

--- src/main/java/xyz/bobkinn/bobsmekanisms/ElectricLampBlock.java
+++ src/main/java/xyz/bobkinn/bobsmekanisms/ElectricLampBlock.java
@@ -4,10 +4,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -16,9 +16,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,6 +23,11 @@ public class ElectricLampBlock extends BaseEntityBlock {
     public ElectricLampBlock(BlockBehaviour.Properties props) {
         super(props.lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0));
         this.registerDefaultState(this.defaultBlockState().setValue(BlockStateProperties.LIT, false));
+    }
+
+    @Override
+    public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
+        return RenderShape.MODEL;
     }
 
     @Override
